1a39dcab8538aed80161b7003b2df1cff20e68ac
Runtime-Error3490 <shaleen.shukla@learner.manipal.edu>
2024-06-12 20:29:34 +0530

    use State Object type

---
+++ use State with Object/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import HookCounterThree from './component/HookCounterThree'
+function App() {
+  return (
+    <>
+    <HookCounterThree/>
+    </>
+  )
+}
+
+export default App
---
+++ use State with Object/src/component/HookCounterThree.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+function HookCounterThree() {
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+  return (
+    <div>
+      <input
+        type="text"
+        value={name.firstName}
+        onChange={(e) => setName({ firstName: e.target.value })}
+      />
+      <input
+        type="text"
+        value={name.lastName}
+        onChange={(e) => setName({ firstName: e.target.value })}
+      />
+      <h2>
+        Your Name is {name.firstName} {name.lastName}
+      </h2>
+    </div>
+  );
+}
+
+export default HookCounterThree;
